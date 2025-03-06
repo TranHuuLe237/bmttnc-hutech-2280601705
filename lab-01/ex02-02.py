615b434a7254e49ada00f89eeb764747df576530
TranHuuLe237 <letran.230704@gmail.com>
2025-03-06 11:27:06 +0700

    Buoi1

---
+++ lab-01/ex02-02.py
@@ -0,0 +1,3 @@
+ban_kinh = float(input("Nhap Ban Kinh: "))
+dientich = 3.14 * (ban_kinh ** 2)
+print("Dien tich hinh tron la: ", dientich)
---
+++ lab-01/ex02-03.py
@@ -0,0 +1,4 @@
+so = int(input("Nhap so nguyen"))
+if so % 2 == 0:
+    print(so, "la so chan")
+print(so, "la so le")
